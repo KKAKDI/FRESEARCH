7eb63b7fb18c705a045a43d9a465130cca2650c7
cuh123 <chldnsgkr7@naver.com>
2019-07-30 09:52:00 +0900

    ResearchController.java 파일 리스트 메소드 추가

--- 2. 개발/FreSearch/src/main/java/org/fs/controller/ResearchController.java
+++ 2. 개발/FreSearch/src/main/java/org/fs/controller/ResearchController.java
@@ -1,17 +1,45 @@
 package org.fs.controller;
 
+import java.util.List;
+
+import org.fs.domain.ResearchVO;
+import org.fs.service.ResearchService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.AllArgsConstructor;
+
 @Controller
-@RequestMapping("/research")
+@RequestMapping("/research/*")
+@AllArgsConstructor
 public class ResearchController {
-	@RequestMapping("/research_list")
-	public String list() {
-		return "research/research_list";
+	
+	private ResearchService service;
+	
+	@GetMapping("research_list")
+	public void list() {}
+	
+	@PostMapping(value = "/research_listTab", 
+		consumes = "application/json",
+	    produces = {
+	          MediaType.APPLICATION_XML_VALUE,
+	          MediaType.APPLICATION_JSON_UTF8_VALUE}) 
+	public ResponseEntity<List<ResearchVO>> listStatus(@RequestBody ResearchVO vo){
+	   return new ResponseEntity<>(service.listStatus(vo), HttpStatus.OK);
 	}
-	@RequestMapping("/research_reg")
-	public String reg() {
-		return "research/research_register";
+	
+	@GetMapping("research_register")
+	public void regForm() {}
+	
+	@PostMapping("research_register")
+	public void reg() {
+		
 	}
+	
 }
